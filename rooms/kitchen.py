eb1c137e05345c1e5ed24afa7c7731ef9294a4e6
JamaicanMoose <jackcollinsnj@gmail.com>
2020-02-11 12:27:08 -0500

    update kitchen & add sandwitch build
    commit 508b34e5f001a533d4881386215b2b69623b5319
    Author: JamaicanMoose <jackcollinsnj@gmail.com>
    Date:   Tue Feb 11 12:23:27 2020 -0500
    
        remove old scenario
    
    commit aba4c2db34ce38d9680453787be939ebaeceaee2
    Author: mohsr <mkrizvi98@gmail.com>
    Date:   Mon Feb 10 22:48:26 2020 -0500
    
        fixed small issue
    
    commit 07961cbe477c8dabd2c20ce6f158abd4bfa79794
    Author: mohsr <mkrizvi98@gmail.com>
    Date:   Mon Feb 10 22:44:55 2020 -0500
    
        fixed help text typo
    
    commit 73914c48fc9794682e7a6e3ad41d9a8db875c53b
    Author: mohsr <mkrizvi98@gmail.com>
    Date:   Mon Feb 10 22:43:25 2020 -0500
    
        finished kitchen
    
    commit 3feceb82142de7997496ccdc9ce02141bb3602f1
    Author: mohsr <mkrizvi98@gmail.com>
    Date:   Mon Feb 10 19:58:09 2020 -0500
    
        moved sandwich build scenario to kitchen, created chef
    
    commit 7ebf5b807d2c342fe72fec2559d02170557da091
    Author: mohsr <mkrizvi98@gmail.com>
    Date:   Mon Feb 10 19:42:05 2020 -0500
    
        modified sandwich building
    
    commit 092d18a34968c14dedc2530c2041c1e846a257dd
    Author: mohsr <mkrizvi98@gmail.com>
    Date:   Mon Feb 10 19:10:51 2020 -0500
    
        sandwich build scenario
    
    commit 78a056eeb42b27ca2fe5a192a9e1ca81330f5412
    Author: mohsr <mkrizvi98@gmail.com>
    Date:   Mon Feb 10 18:26:11 2020 -0500
    
        working on build_sandwich

--- rooms/kitchen.py
+++ rooms/kitchen.py
@@ -1,5 +1,7 @@
 from items.item import Item
 from items.mixins import Entrance
+from person import Person
+from scenarios.scenario import Scenario
 
 """ The Kitchen
 """
@@ -11,19 +13,139 @@ class RecRoomDoor(Entrance, Item):
 
 class spaceBacon(Item):
     name = 'bacon'
-    description = ('\"Space Bacon!(tm) Made with RealPig*!\" \n\n'+
+    description = ('\"SpaceBacon!(tm) Made with RealPig*!\" \n\n'+
                    '*RealPig is a registered trademark of SpaceBacon,'+
                    ' Incorporated. Product may not contain pork products.\n')
 
+class SandwichBuild(Scenario):
+
+    def has_ingredients(self, state):
+        lettuce    = False
+        bread      = False
+        spacebacon = False
+
+        for item in _game_state['player'].state['inventory']:
+            if 'lettuce' in item.name:
+                lettuce = True
+            elif 'bread' in item.name:
+                bread = True
+            elif 'spacebacon' in item.name:
+                spacebacon = True
+
+        return (lettuce and bread and spacebacon)
+
+    def alt_convo(self, state):
+        print("The chef whistles to himself as he stares at a notepad on "
+              "the countertop, listening to some music on his SpacePods.\n"
+              "He doesn't seem to notice you, but you notice, in large letters,"
+              " the word \"BEANS?\" written at the top of the page.")
+
+    def start(self, state):
+        # Run alt scenario if you don't have ingredients
+        if not self.has_ingredients(state):
+            self.alt_convo(state)
+            return
+
+        # Begin the initial scenario
+        sandwich = {"open-faced": False,
+                    "cut": "",
+                    "sauce": ""}
+
+        def _n():
+            print("Arming yourself with bread, lettuce, and SpaceBacon, "
+                  "you are now ready to realize your ultimate goal.")
+            sleep(1)
+            print("\n\"Woohoo!\" shouts the chef, pulling out his SpacePods "
+                  "and raising his arms in the air.")
+            sleep(1)
+            print("\nYou begin to assemble the sandwich...")
+            sleep(1)
+
+            print("Not so fast! What kind of sandwich is this, anyhow?")
+            sandwich["open-faced"] = (Scenario.pick(["A normal sandwich", 
+                                                     "Open-faced"]) == 1)
+
+            print("With an image of the sandwich in mind, you begin to "
+                  "assemble it.\nYou slap a slice of bread on the chrome "
+                  "countertop, layering it with crisp lettuce and a juicy slab "
+                  "of SpaceBacon.")
+            sleep(2)
+            print("Using the tools at your disposal, you prepare to cut the "
+                  "sandwich...")
+            sleep(1)
+            print("...but a shriek pierces the air!")
+            sleep(1)
+            print("\"Wait!!\" shouts the chef, \"You forgot the sauce!\"\n"
+                  "He gestures at the two labeled bottles to your left.")
+            sleep(1)
+
+            print("Which sauce do you choose?")
+            choices = ["Plurpel Sauce®", "Salt", "No sauce"]
+            sandwich["sauce"] = choices[Scenario.pick(choices)]
+            if sandwich["sauce"] == "Salt":
+                sandwich["sauce"] = "salt"
+                print("You choose the bottle labeled \"Salt.\" As far as your "
+                      "knowledge of salt goes, this is not it, but you don't "
+                      "want to make a fuss.")
+                sleep(1)
+            elif sandwich["sauce"] == "No sauce":
+                sandwich["sauce"] = "sauceless"
+                print("The chef looks on as horror as you stare at him and "
+                      "proceed, ignoring the sauces...")
+                sleep(1)
+            else:
+                print("The chef smiles, seemingly approving of your sauce "
+                      "choice, as you pour the mysteriously-colored sauce on "
+                      "your sandwich.")
+                sleep(1)
+
+            print("Now, how are you cutting this sandwich anyway?")
+            sandwich["cut"] = Scenario.pick(["Triangles", "In half"])
+            if sandwich["cut"] == "Triangles":
+                _game_state['player'].add_accolade('Triangular')
+            else:
+                _game_state['player'].add_accolade('Rectangular')
+
+            if sandwich["open-faced"]:
+                print("You cut your sandwich as you see fit. You are now the "
+                      "proud owner of an open-faced %s sandwich!" % sandwich["sauce"])
+            else:
+                print("You cut your sandwich as you see fit. You are now the "
+                      "proud owner of a %s sandwich!" % sandwich["sauce"])
+
+            sleep(1)
+            print("...")
+            sleep(1)
+            print("...suddenly, you catch yourself off guard, and are briefly "
+                  "blinded by the shining chrome room...")
+            sleep(1)
+            print("...")
+            sleep(1)
+            print("...after regaining your senses, your sandwich is gone!\n"
+                  "You see a butler walking through the door behind you, "
+                  "your sandwich sitting on an illustrious silver platter "
+                  "in his arms.")
+            _game_state['player'].add_title('Epicurious')
+
+        # Run the scenario    
+        _n()
+
+class Chef(Person):
+    name = 'chef'
+    scenario = SandwichBuild()
+    description = f'''\
+His head is adorned with a puffy white hat, and he's wearing a stained apron.'''
+    alt_names = []
 
 kitchen = {
     'description': ('You are in the KITCHEN.\nThe room smells wonderful,'+
                     'and the shelves are stocked full with all manner of'+
                     ' culinary delights.\nSomeone has left a plate of SPACE'+
-                    ' BACON here - surely they wouldn\'t ha.'),
+                    ' BACON here.\nYou see a CHEF looking at a notepad and'+
+                    ' jamming to music.'),
     'items': [
         spaceBacon(),],
-    'people': [],
+    'people': [Chef()],
     'exits': {
         'hubwards': RecRoomDoor(),
     }
--- scenarios/scenario.py
+++ scenarios/scenario.py
@@ -21,5 +21,25 @@ class Scenario():
                 print('That is not one of your choices.')
         choices[choice][1]()
 
+    # Pick an option, this is just choose without branching
+    @staticmethod
+    def pick(choices):
+        for i, choice in enumerate(choices):
+            print(f'[{i}] : {choice}')
+
+        choice = None
+        while choice not in range(0,len(choices)):
+            try:
+                choice_text = input('--? ')
+                if choice_text in ('q', 'quit'):
+                    exit(0)
+                print('')
+                choice = int(choice_text)
+                if choice not in range(0,len(choices)):
+                    raise ValueError()
+            except ValueError:
+                print('That is not one of your choices.')
+        return choice
+
     def start(self, state):
         pass
