74133ca67b9b8b308da539e7b06d510e37aba38b
mohsr <mkrizvi98@gmail.com>
2020-02-11 15:32:06 -0500

    finished sandwich ending

--- rooms/kitchen.py
+++ rooms/kitchen.py
@@ -2,6 +2,8 @@ from items.item import Item
 from items.mixins import Entrance
 from person import Person
 from scenarios.scenario import Scenario
+from time import sleep
+from sys import exit
 
 """ The Kitchen
 """
@@ -19,6 +21,23 @@ class spaceBacon(Item):
 
 class SandwichBuild(Scenario):
 
+    class sandwich(Item):
+        name = 'sandwich'
+        description = ''
+
+        def __init__(self, sandwichState):
+            if sandwichState['open-faced']:
+                self.description = ('A delicious looking open-faced %s '
+                                    'sandwich, cut into two scrumptious %s' % 
+                                    (sandwichState['sauce'], 
+                                     sandwichState['cut']))
+            else:
+                self.description = ('A delicious looking %s sandwich, '
+                                    'cut into two scrumptious %s' % 
+                                    (sandwichState['sauce'], 
+                                     sandwichState['cut']))
+
+
     def has_ingredients(self, state):
         lettuce    = False
         bread      = False
@@ -29,10 +48,23 @@ class SandwichBuild(Scenario):
                 lettuce = True
             elif 'bread' in item.name:
                 bread = True
-            elif 'spacebacon' in item.name:
+            elif 'bacon' in item.name:
                 spacebacon = True
 
         return (lettuce and bread and spacebacon)
+
+    def remove_ingredients(self, state):
+        # Bad workaround to deal with array iteration issue: just loop over
+        # the array three times
+        for item in _game_state['player'].state['inventory']:
+            if 'lettuce' in item.name:
+                _game_state['player'].state['inventory'].remove(item)
+        for item in _game_state['player'].state['inventory']:
+            if 'bacon' in item.name:
+                _game_state['player'].state['inventory'].remove(item)
+        for item in _game_state['player'].state['inventory']:
+            if 'bread' in item.name:
+                _game_state['player'].state['inventory'].remove(item)
 
     def alt_convo(self, state):
         print("The chef whistles to himself as he stares at a notepad on "
@@ -101,31 +133,65 @@ class SandwichBuild(Scenario):
 
             print("Now, how are you cutting this sandwich anyway?")
             sandwich["cut"] = Scenario.pick(["Triangles", "In half"])
-            if sandwich["cut"] == "Triangles":
-                _game_state['player'].add_accolade('Triangular')
+            if sandwich["cut"] == 0:
+                sandwich["cut"] = "triangles"
+                _game_state['player'].add_accolade('The Triangular')
             else:
-                _game_state['player'].add_accolade('Rectangular')
+                sandwich["cut"] = "rectangles"
+                _game_state['player'].add_accolade('The Rectangular')
 
             if sandwich["open-faced"]:
-                print("You cut your sandwich as you see fit. You are now the "
-                      "proud owner of an open-faced %s sandwich!" % sandwich["sauce"])
+                print("\nYou cut your sandwich as you see fit. You are now the "
+                      "proud owner of an open-faced %s sandwich!\n" % 
+                      sandwich["sauce"])
             else:
-                print("You cut your sandwich as you see fit. You are now the "
-                      "proud owner of a %s sandwich!" % sandwich["sauce"])
+                print("\nYou cut your sandwich as you see fit. You are now the "
+                      "proud owner of a %s sandwich!\n" % sandwich["sauce"])
+            self.remove_ingredients(state)
 
-            sleep(1)
-            print("...")
-            sleep(1)
-            print("...suddenly, you catch yourself off guard, and are briefly "
-                  "blinded by the shining chrome room...")
-            sleep(1)
-            print("...")
-            sleep(1)
-            print("...after regaining your senses, your sandwich is gone!\n"
-                  "You see a butler walking through the door behind you, "
-                  "your sandwich sitting on an illustrious silver platter "
-                  "in his arms.")
-            _game_state['player'].add_title('Epicurious')
+            # Add the sandwich to your inventory
+            _game_state['player'].state['inventory'].append(
+                self.sandwich(sandwich))
+
+            # sleep(1)
+            # print("...")
+            # sleep(1)
+            # print("...suddenly, you catch yourself off guard, and are briefly "
+            #       "blinded by the shining chrome room...")
+            # sleep(1)
+            # print("...")
+            # sleep(1)
+            # print("...after regaining your senses, your sandwich is gone!\n"
+            #       "You see a butler walking through the door behind you, "
+            #       "your sandwich sitting on an illustrious silver platter "
+            #       "in his arms.")
+            _game_state['player'].add_title('Epicureous')
+
+            # End the game if the player has achieved their goal
+            if _game_state['player'].has_accolade('The Hungry'):
+                print("\nAs you bite into your sandwich, a feeling of peace "
+                      "washes over you...")
+                sleep(1)
+                print("...pure, unadulterated bliss...")
+                sleep(1)
+                print("...the way the %s melts into the SpaceBacon can only "
+                      "be described as heavenly." % sandwich["sauce"])
+                sleep(1)
+                print("All your worries wash away, and you don't have a care "
+                      "in the world.\nAll that matters is you and your "
+                      "sandwich.")
+                sleep(3)
+
+                print("\n\nThank you for playing our game! Enter \"0\" when "
+                      "you are ready to exit.")
+                Scenario.choose([("Bye!", lambda: exit(0))])
+            else:
+                print("\nAs you bite into your sandwich, you marvel at how "
+                      "good it tastes!")
+                sleep(1)
+                print("With this sandwich at your side, you feel "
+                      "well-equipped to tackle your ultimate goal!")
+
 
         # Run the scenario    
         _n()
